52b7629bd7047d00f8b2297bc4715bc1b9709ff8
Bruno S Marques <brunosmarques1995@hotmail.com>
2023-10-11 15:03:52 -0300

    added new arg to dxc

--- installers/dxc.bat
+++ installers/dxc.bat
@@ -7,7 +7,7 @@ IF STR(%~2) NEQ STR() ( GOTO:passed_install )
 goto:eof
 
 :passed_install
-cmake -S ./modules/dxc -B ./dependencies/dxc -G Ninja -C ./modules/dxc/cmake/caches/PredefinedParams.cmake -DCMAKE_BUILD_TYPE=%~1 -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_INSTALL_PREFIX=%~2
+cmake -S ./modules/dxc -B ./dependencies/dxc -G Ninja -C ./modules/dxc/cmake/caches/PredefinedParams.cmake -DCMAKE_BUILD_TYPE=%~1 -DENABLE_SPIRV_CODEGEN=ON -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_INSTALL_PREFIX=%~2
 cmake --build ./dependencies/dxc --config %~1 --target install-dxc-headers
 cmake --build ./dependencies/dxc --config %~1 --target install-dxc
 cmake --build ./dependencies/dxc --config %~1 --target install-dxcompiler
